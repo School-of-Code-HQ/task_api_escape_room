51fb139a2696fb00fd90dadc8266de2fa536b0e0
cwissy <meahchris@gmail.com>
2021-04-29 12:10:02 +0100

    productionising middleware

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
+const cors = require("cors");
 
-var indexRouter = require("./routes/index");
+const indexRouter = require("./routes/index");
 
-var app = express();
+const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
+app.disable("x-powered-by");
+app.use(helmet());
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
