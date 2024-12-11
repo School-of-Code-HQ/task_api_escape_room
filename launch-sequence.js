7b66ed1c5eaae23c2e5fa27254c0b496dadfd36f
cwissy <chris@schoolofcode.co.uk>
2024-12-11 11:38:34 +0000

    updates without answers

--- launch-sequence.js
+++ launch-sequence.js
@@ -3,18 +3,6 @@ async function submitCode() {
   try {
     // Your investigation code should go here
     // Leave your lines of code in when you find something out, so that you can always come back to it and see how you got there
-    const response = await fetch("/api/codes", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        enter: "ROVER",
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
-    document.getElementById("launch").src = data.img;
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
